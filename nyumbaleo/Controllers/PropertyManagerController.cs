39328a39111ac7ff6c708d12a2ac9d3ee0d65e6d
sifa wanjiru <simemaw30@gmail.com>
2013-09-14 16:26:42 +1000

    Added PropertyManager

---
+++ nyumbaleo/Controllers/PropertyManagerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace nyumbaleo.Controllers
+{
+    public class PropertyManagerController : Controller
+    {
+        //
+        // GET: /PropertyManager/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
